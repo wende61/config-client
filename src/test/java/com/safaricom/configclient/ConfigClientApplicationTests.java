021a45846e780a6ea877328466c6c39bca96cf19
Wendafrash Buzuayehu <wendebuzu@gmail.com>
2024-04-18 00:53:09 +0300

    first commit

---
+++ src/test/java/com/safaricom/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safaricom.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
